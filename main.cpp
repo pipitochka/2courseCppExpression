fcf41d2f583ef8145fc2ba0d851a55c08fd6cdcc
pipitochka <aaakulov_3@edu.hse.ru>
2025-02-23 00:14:49 +0300

    fix shared ptr

--- main.cpp
+++ main.cpp
@@ -6,9 +6,12 @@ int main() {
         std::make_shared<VarExpression>(VarExpression("x")),
         plus_op);
 
+    std::map<std::string, int> map = {{"x", 3}}; 
     std::shared_ptr<Expression> derivative = expression.diff();
-
+    double derivative2 = expression.eval(map);
     auto binaryDerivative = std::dynamic_pointer_cast<BinaryExpression>(derivative);
 
     std::cout << binaryDerivative->toString() << std::endl;
+
+    std::cout << derivative2 << std::endl;
 }
--- src/VarExpression.cpp
+++ src/VarExpression.cpp
@@ -1,7 +1,7 @@
 #include "../inc/Expression.h"
 
 std::shared_ptr<Expression> VarExpression::diff() {
-    return std::make_unique<ConstantExpression>(ConstantExpression(1));
+    return std::make_shared<ConstantExpression>(ConstantExpression(1));
 }
 
 double VarExpression::eval(std::map<std::string, int> &map) {
