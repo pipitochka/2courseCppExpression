422b7d6b761f0a3c9e2d22c28a1d6e8364e91532
pipitochka <aaakulov_3@edu.hse.ru>
2025-02-26 08:29:31 +0300

    minor optimizations

--- src/Optimization.cpp
+++ src/Optimization.cpp
@@ -1,7 +1,6 @@
 #include "../inc/Expression.h"
 
 std::shared_ptr<Expression> optimize(std::shared_ptr<Expression> expr){
-    auto expr_mono = dynamic_cast<MonoExpression*>(expr.get());
     if (auto expr_binary = dynamic_cast<BinaryExpression*>(expr.get())) {
         expr_binary->lhs = optimize(expr_binary->lhs);
         expr_binary->rhs = optimize(expr_binary->rhs);
@@ -18,6 +17,7 @@ std::shared_ptr<Expression> optimize(std::shared_ptr<Expression> expr){
                 if (rhs_const && rhs_const->getValue() == 0) {
                     return expr_binary->lhs;
                 }
+                break;
             }
             case minus_op: {
                 if (lhs_const && rhs_const) {
@@ -33,6 +33,7 @@ std::shared_ptr<Expression> optimize(std::shared_ptr<Expression> expr){
                         mul_op
                     );
                 }
+                break;
             }
             case mul_op: {
                 if (lhs_const && rhs_const) {
@@ -47,6 +48,7 @@ std::shared_ptr<Expression> optimize(std::shared_ptr<Expression> expr){
                 if (rhs_const && rhs_const->getValue() == 1) {
                     return expr_binary->lhs;
                 }
+                break;
             }
             case div_op: {
                 if (rhs_const && rhs_const->getValue() == 0) {
@@ -58,6 +60,7 @@ std::shared_ptr<Expression> optimize(std::shared_ptr<Expression> expr){
                 if (lhs_const && lhs_const->getValue() == 0) {
                     return std::make_shared<ConstantExpression>(0);
                 }
+                break;
             }
             case exp_op: {
                 if (lhs_const && lhs_const->getValue() == 0) {
@@ -69,6 +72,22 @@ std::shared_ptr<Expression> optimize(std::shared_ptr<Expression> expr){
                 if (lhs_const && rhs_const) {
                     return std::make_shared<ConstantExpression>(std::pow(lhs_const->getValue(), rhs_const->getValue()));
                 }
+                break;
+            }
+        }
+    }
+    if (auto expr_mono = dynamic_cast<MonoExpression*>(expr.get())) {
+        if (auto val = dynamic_cast<ConstantExpression *>(expr_mono->expr.get())) {
+            expr_mono->expr = optimize(expr_mono->expr);
+            switch (expr_mono->function) {
+                case sin_func:
+                    return std::make_shared<ConstantExpression>(std::sin(val->getValue()));
+                case cos_func:
+                    return std::make_shared<ConstantExpression>(std::cos(val->getValue()));
+                case exp_func:
+                    return std::make_shared<ConstantExpression>(std::exp(val->getValue()));
+                case ln_func:
+                    return std::make_shared<ConstantExpression>(std::log(val->getValue()));
             }
         }
     }
