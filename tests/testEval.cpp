55a4a90cc8a4b286411f70038c6f8e86c099251f
pipitochka <aaakulov_3@edu.hse.ru>
2025-03-07 00:44:43 +0300

    add templates

--- tests/testEval.cpp
+++ tests/testEval.cpp
@@ -1,13 +1,25 @@
 #include <cassert>
+#include <complex>
 #include <iostream>
 #include <ostream>
 
 #include "../inc/Expression.h"
 #include "../inc/Parser.h"
 
-void TestEval(std::string input, double expected, std::map<std::string, double> params) {
+void TestEvalDouble(std::string input, double expected, std::map<std::string, double> params) {
     auto tmp = tokenize(input);
-    auto result = parseExpression(tmp.get());
+    auto result = parseExpression<double>(tmp.get());
+    auto qq = result->eval(params);
+    std::cout << input << std::endl;
+    std::cout << qq << std::endl;
+    std::cout << expected << std::endl;
+    std::cout << std::endl;
+    assert(qq == expected);
+}
+
+void TestEvalComplex(std::string input, std::complex<double> expected, std::map<std::string, double> params) {
+    auto tmp = tokenize(input);
+    auto result = parseExpression<std::complex<double>>(tmp.get());
     auto qq = result->eval(params);
     std::cout << input << std::endl;
     std::cout << qq << std::endl;
@@ -17,17 +29,30 @@ void TestEval(std::string input, double expected, std::map<std::string, double> 
 }
 
 int main() {
-    TestEval("5 + 7", 12, {});
-    TestEval("2.5", 2.5, {});
-    TestEval("x + 5", 7, {{"x", 2}});
-    TestEval("x ^ 5", 32, {{"x", 2}});
-    TestEval("x + y", 12, {{"x", 5}, {"y", 7}});
-    TestEval("x ^ y", 125, {{"x", 5}, {"y", 3}});
-    TestEval("x / y", 2, {{"x", 10}, {"y", 5}});
-    TestEval("sin(x)", 0, {{"x", 0}, {"y", 5}});
-    TestEval("cos(x)", 1, {{"x", 0}});  
-    TestEval("exp(x)", exp(1), {{"x", 1}});
-    TestEval("x * sin(x)", 3 * sin(3.0), {{"x", 3}});
-    TestEval("x * y + sin(x)", 5 * 2 + sin(5), {{"x", 5}, {"y", 2}});
+    TestEvalDouble("5 + 7", 12, {});
+    TestEvalDouble("2.5", 2.5, {});
+    TestEvalDouble("x + 5", 7, {{"x", 2}});
+    TestEvalDouble("x ^ 5", 32, {{"x", 2}});
+    TestEvalDouble("x + y", 12, {{"x", 5}, {"y", 7}});
+    TestEvalDouble("x ^ y", 125, {{"x", 5}, {"y", 3}});
+    TestEvalDouble("x / y", 2, {{"x", 10}, {"y", 5}});
+    TestEvalDouble("sin(x)", 0, {{"x", 0}, {"y", 5}});
+    TestEvalDouble("cos(x)", 1, {{"x", 0}});  
+    TestEvalDouble("exp(x)", exp(1), {{"x", 1}});
+    TestEvalDouble("x * sin(x)", 3 * sin(3.0), {{"x", 3}});
+    TestEvalDouble("x * y + sin(x)", 5 * 2 + sin(5), {{"x", 5}, {"y", 2}});
+
+    TestEvalComplex("5 + 7", std::complex<double>(12,0), {});
+    TestEvalComplex("2.5", std::complex<double>(2.5,0), {});
+    TestEvalComplex("x + 5", std::complex<double>(7,0), {{"x", 2}});
+    TestEvalComplex("x ^ 5", std::complex<double>(32,0), {{"x", 2}});
+    TestEvalComplex("x + y", std::complex<double>(12,0), {{"x", 5}, {"y", 7}});
+    //TestEvalComplex("x ^ y", std::complex<double>(125,0), {{"x", 5}, {"y", 3}});
+    TestEvalComplex("x / y", 2, {{"x", 10}, {"y", 5}});
+    TestEvalComplex("sin(x)", 0, {{"x", 0}, {"y", 5}});
+    TestEvalComplex("cos(x)", 1, {{"x", 0}});  
+    TestEvalComplex("exp(x)", exp(1), {{"x", 1}});
+    TestEvalComplex("x * sin(x)", 3 * sin(3.0), {{"x", 3}});
+    TestEvalComplex("x * y + sin(x)", 5 * 2 + sin(5), {{"x", 5}, {"y", 2}});
 }
 
--- tests/testInput.cpp
+++ tests/testInput.cpp
@@ -1,13 +1,25 @@
 #include <cassert>
+#include <complex>
 #include <iostream>
 #include <ostream>
+#include <string>
 
 #include "../inc/Expression.h"
 #include "../inc/Parser.h"
 
-void TestInput(std::string input, std::string expected) {
+void TestInputDouble(std::string input, std::string expected) {
     auto tmp = tokenize(input);
-    auto result = parseExpression(tmp.get());
+    auto result = parseExpression<double>(tmp.get());
+    std::cout << input << std::endl;
+    std::cout << result->toString() << std::endl;
+    std::cout << expected << std::endl;
+    std::cout << std::endl;
+    assert(result->toString() == expected);
+}
+
+void TestInputComplex(std::string input, std::string expected) {
+    auto tmp = tokenize(input);
+    auto result = parseExpression<std::complex<double>>(tmp.get());
     std::cout << input << std::endl;
     std::cout << result->toString() << std::endl;
     std::cout << expected << std::endl;
@@ -16,25 +28,47 @@ void TestInput(std::string input, std::string expected) {
 }
 
 int main() {
-    TestInput("x", "x");
-    TestInput("x + y", "(x + y)");
-    TestInput("sin(cos(x + y))", "sin(cos((x + y)))");
-    TestInput("2 + 3", "(2.000000 + 3.000000)");
-    TestInput("2 + 3 * 5", "(2.000000 + (3.000000 * 5.000000))");
-    TestInput("1 * 2 + 3 * 4", "((1.000000 * 2.000000) + (3.000000 * 4.000000))");
-    TestInput("2 ^ 3 + 4", "((2.000000 ^ 3.000000) + 4.000000)");
-    TestInput("2 ^ 3 * 5 + 4", "(((2.000000 ^ 3.000000) * 5.000000) + 4.000000)");
-    TestInput("2 ^ (3 * 5) + 4", "((2.000000 ^ (3.000000 * 5.000000)) + 4.000000)");
-    TestInput("x ^ y + t", "((x ^ y) + t)");
-    TestInput("a * (b * c + d)", "(a * ((b * c) + d))");
-    TestInput("z ^ (x ^ y + t)", "(z ^ ((x ^ y) + t))");
-    TestInput("2 + 3 * 4", "(2.000000 + (3.000000 * 4.000000))");
-    TestInput("2 * 3 + 4", "((2.000000 * 3.000000) + 4.000000)");
-    TestInput("2 + 3 * sin(cos(x + y))", "(2.000000 + (3.000000 * sin(cos((x + y)))))");
-    TestInput("2 * ln(x) + 3", "((2.000000 * ln(x)) + 3.000000)");
-    TestInput("2 * (ln(x)) + 3 * sin(cos(x + y))", "((2.000000 * ln(x)) + (3.000000 * sin(cos((x + y)))))");
-    TestInput("x ^ ln(y) + exp(z) ^ (cos(t) + sin(m))", "((x ^ ln(y)) + (exp(z) ^ (cos(t) + sin(m))))");
-    TestInput("a / b * c", "((a / b) * c)");
-    TestInput("a * b / c", "((a * b) / c)");
+    TestInputDouble("x", "x");
+    TestInputDouble("x + y", "(x + y)");
+    TestInputDouble("sin(cos(x + y))", "sin(cos((x + y)))");
+    TestInputDouble("2 + 3", "(2 + 3)");
+    TestInputDouble("2 + 3 * 5", "(2 + (3 * 5))");
+    TestInputDouble("1 * 2 + 3 * 4", "((1 * 2) + (3 * 4))");
+    TestInputDouble("2 ^ 3 + 4", "((2 ^ 3) + 4)");
+    TestInputDouble("2 ^ 3 * 5 + 4", "(((2 ^ 3) * 5) + 4)");
+    TestInputDouble("2 ^ (3 * 5) + 4", "((2 ^ (3 * 5)) + 4)");
+    TestInputDouble("x ^ y + t", "((x ^ y) + t)");
+    TestInputDouble("a * (b * c + d)", "(a * ((b * c) + d))");
+    TestInputDouble("z ^ (x ^ y + t)", "(z ^ ((x ^ y) + t))");
+    TestInputDouble("2 + 3 * 4", "(2 + (3 * 4))");
+    TestInputDouble("2 * 3 + 4", "((2 * 3) + 4)");
+    TestInputDouble("2 + 3 * sin(cos(x + y))", "(2 + (3 * sin(cos((x + y)))))");
+    TestInputDouble("2 * ln(x) + 3", "((2 * ln(x)) + 3)");
+    TestInputDouble("2 * (ln(x)) + 3 * sin(cos(x + y))", "((2 * ln(x)) + (3 * sin(cos((x + y)))))");
+    TestInputDouble("x ^ ln(y) + exp(z) ^ (cos(t) + sin(m))", "((x ^ ln(y)) + (exp(z) ^ (cos(t) + sin(m))))");
+    TestInputDouble("a / b * c", "((a / b) * c)");
+    TestInputDouble("a * b / c", "((a * b) / c)");
+
+
+    TestInputComplex("x", "x");
+    TestInputComplex("x + y", "(x + y)");
+    TestInputComplex("sin(cos(x + y))", "sin(cos((x + y)))");
+    TestInputComplex("2 + 3", "((2,0) + (3,0))");
+    TestInputComplex("2 + 3 * 5", "((2,0) + ((3,0) * (5,0)))");
+    TestInputComplex("1 * 2 + 3 * 4", "(((1,0) * (2,0)) + ((3,0) * (4,0)))");
+    TestInputComplex("2 ^ 3 + 4", "(((2,0) ^ (3,0)) + (4,0))");
+    TestInputComplex("2 ^ 3 * 5 + 4", "((((2,0) ^ (3,0)) * (5,0)) + (4,0))");
+    TestInputComplex("2 ^ (3 * 5) + 4", "(((2,0) ^ ((3,0) * (5,0))) + (4,0))");
+    TestInputComplex("x ^ y + t", "((x ^ y) + t)");
+    TestInputComplex("a * (b * c + d)", "(a * ((b * c) + d))");
+    TestInputComplex("z ^ (x ^ y + t)", "(z ^ ((x ^ y) + t))");
+    TestInputComplex("2 + 3 * 4", "((2,0) + ((3,0) * (4,0)))");
+    TestInputComplex("2 * 3 + 4", "(((2,0) * (3,0)) + (4,0))");
+    TestInputComplex("2 + 3 * sin(cos(x + y))", "((2,0) + ((3,0) * sin(cos((x + y)))))");
+    TestInputComplex("2 * ln(x) + 3", "(((2,0) * ln(x)) + (3,0))");
+    TestInputComplex("2 * (ln(x)) + 3 * sin(cos(x + y))", "(((2,0) * ln(x)) + ((3,0) * sin(cos((x + y)))))");
+    TestInputComplex("x ^ ln(y) + exp(z) ^ (cos(t) + sin(m))", "((x ^ ln(y)) + (exp(z) ^ (cos(t) + sin(m))))");
+    TestInputComplex("a / b * c", "((a / b) * c)");
+    TestInputComplex("a * b / c", "((a * b) / c)");
 }
 
